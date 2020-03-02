6927ea21b15360089c29e6944e97983ce4f196d9
liliakarp <lillianmoon116@gmail.com>
2020-03-02 22:34:08 +0600

    Project with header in process

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	//$('#my-menu').mmenu();
+
+/*document.addEventListener(
+        "DOMContentLoaded", () => {
+            new Mmenu( "#my-menu" );
+        }
+    );*/
+
+	$("#my-menu").mmenu();
+
+});
